a82ede7642d4e2ce9635d88bdf5b8f4022716677
Alexey Sultrekov <5672013+arhix@users.noreply.github.com>
2024-06-20 23:57:33 -0500

    second?

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # OttoDIY
+
+# Body
+3D model: [Otto DIY](https://www.printables.com/model/31955-otto-diy-build-your-own-robot)
+
+# Hardware
+- 4x **SG90** (legs)
+- **PCA9685** 16-Channel Servo Driver ([documentation](https://learn.adafruit.com/16-channel-pwm-servo-driver/hooking-it-up))
+- **Buzzer** (too loud, cover with sticker)
+- **Arduino Uno** (need to replace with smth smaller)
+- **ultrasonic sensor** (eyes)
+- Jumper wires
--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #define N_SERVOS 4 // Number of servos
 #define INTERVALTIME 10.0
 
-Otto Otto;  //This is Otto!
+Otto Otto; // This is Otto!
 
 #define LeftLeg 1
 #define LeftFoot 0
@@ -14,7 +14,7 @@ Otto Otto;  //This is Otto!
 #define RightFoot 2
 #define Buzzer 13
 #define Trigger 8 // ultrasonic sensor trigger pin
-#define Echo 9 // ultrasonic sensor echo pin
+#define Echo 9    // ultrasonic sensor echo pin
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
@@ -26,36 +26,39 @@ void setup()
     pwm.setPWMFreq(60);
 
     Otto.init(pwm, LeftLeg, RightLeg, LeftFoot, RightFoot, Buzzer);
-    pinMode(Trigger, OUTPUT); 
-    pinMode(Echo, INPUT); 
+    pinMode(Trigger, OUTPUT);
+    pinMode(Echo, INPUT);
 
     delay(10);
 }
 
-long ultrasound() {
-   long duration, distance;
-   digitalWrite(Trigger,LOW);
-   delayMicroseconds(2);
-   digitalWrite(Trigger, HIGH);
-   delayMicroseconds(10);
-   digitalWrite(Trigger, LOW);
-   duration = pulseIn(Echo, HIGH);
-   distance = duration/58;
-   return distance;
+long ultrasound()
+{
+    long duration, distance;
+    digitalWrite(Trigger, LOW);
+    delayMicroseconds(2);
+    digitalWrite(Trigger, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(Trigger, LOW);
+    duration = pulseIn(Echo, HIGH);
+    distance = duration / 58;
+    return distance;
 }
 
 void loop()
 {
     int ult = ultrasound();
     Serial.println(ult);
-    if (ult <= 15) {
-      Otto.sing(S_surprise);
-      Otto.playGesture(OttoConfused);
-      Otto.walk(2,1000,-1); // BACKWARD x2
-      Otto.turn(3,1000,1); // LEFT x3
-        // delay(500);
+
+    if (ult <= 15)
+    {
+        Otto.sing(S_surprise);
+        Otto.playGesture(OttoConfused);
+        Otto.walk(2, 1000, -1); // BACKWARD x2
+        Otto.turn(3, 1000, 1);  // LEFT x3
+                               // delay(500);
     }
 
     Serial.println("walk");
-    Otto.walk(1,1000,1); // FORWARD x1
+    Otto.walk(1, 1000, 1); // FORWARD x1
 }
